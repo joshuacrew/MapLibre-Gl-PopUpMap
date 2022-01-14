0ad8af739501afeac7d7644c55ca85c89c2ab754
Joshua Crew <joshua.crew@superawesome.com>
2022-01-14 13:38:19 +0000

    initial app

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+// NOTE: To use this example standalone (e.g. outside of repo)
+// delete the local development overrides at the bottom of this file
+
+// avoid destructuring for older Node version support
+const resolve = require('path').resolve;
+const webpack = require('webpack');
+
+const config = {
+  mode: 'development',
+
+  devServer: {
+    static: '.'
+  },
+
+  entry: {
+    app: resolve('./src/app')
+  },
+
+  output: {
+    library: 'App'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      'mapbox-gl': 'maplibre-gl'
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        include: [resolve('.')],
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/env', '@babel/react']
+            }
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+
+  // Optional: Enables reading mapbox token from environment variable
+  plugins: [new webpack.EnvironmentPlugin({ MapboxAccessToken: '' })]
+};
+
+module.exports = config;
